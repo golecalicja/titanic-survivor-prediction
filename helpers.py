e2bef51063ffa7ac69024a51e43173ede128ef81
golecalicja <jJ8ktZsxE7kb8DU>
2021-11-30 15:00:56 +0100

    added helpers.py

--- helpers.py
+++ helpers.py
@@ -1,10 +1,6 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import style
 import seaborn as sns
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
 
